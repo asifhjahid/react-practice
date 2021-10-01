a640c56b3e6d959ec027b44f42a1a22f5e9b65c8
asifhjahid <dhrubo8211diu@gmail.com>
2021-10-02 00:49:36 +0600

    useState hook

--- src/App.js
+++ src/App.js
@@ -1,45 +1,15 @@
-import React from 'react';
-import ClickCounter from './components/ClickCounter';
-import Counter from './components/Counter';
-import Section from './components/Section';
-import ThemeContext from './context/ThemeContext';
+import React from 'react'
+import Counter from './components/hooks/useState/Counter'
+// import NewTodo from './components/hooks/useState/NewTodo'
+// import Todo from './components/hooks/useState/Todo'
 
 
-
-
-export default class App extends React.Component{
-  state = {
-    theme:'dark'
-  }
-  switchTheme = ()=>{
-    this.setState(({theme})=>{
-        if(theme ==='dark'){
-            return {
-                theme:'light',
-            }
-
-        }else{
-            return{
-                theme:'dark'
-            }
-        }
-    })
-  }
-  render(){
-    const{theme} = this.state;
-    return (
-      <div>
-       
-        <Counter>
-        {(count,incrementCount)=> (
-        <ClickCounter counter={count} incrementCount={incrementCount} />
-        )}
-        </Counter>
-  
-       <ThemeContext.Provider value={{theme:theme,switchTheme:this.switchTheme}}> 
-        <Section  />
-        </ThemeContext.Provider>
-      </div>
-    )
-  }
+export default function App() {
+  return (
+    <div>
+      {/* <Todo /> */}
+      {/* <NewTodo /> */}
+      <Counter />
+    </div>
+  )
 }
